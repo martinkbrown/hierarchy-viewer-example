bef72eee3297d69ac66fa44de88a3126ef80b1a8
Martin Brown <martijnetje@gmail.com>
2012-07-17 17:09:19 -0400

    initial commit

---
+++ src/co/martinbrown/example/hierarchyviewer/ViewHierarchyExampleActivity.java
@@ -0,0 +1,13 @@
+package co.martinbrown.example.hierarchyviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewHierarchyExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
